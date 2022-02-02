d7de4b5896856b382626bb3caa57baa2949a0684
hamad <hamad_1995_1995@outlook.sa>
2022-02-02 06:56:33 +0300

    the first commit

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,40 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-building"></i> Branch</a>
+<ul>
+<li><a href="create-branch.php">Create Branch</a></li>
+<li><a href="manage-branches.php">Manage Branchs</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-usd" aria-hidden="true"></i> Offers</a>
+					<ul>
+						<li><a href="post-offer.php">ADD Offer</a></li>
+						<li><a href="manage-offers.php">Manage Offers</a></li>
+					</ul>
+				</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
+					<ul>
+						<li><a href="new-bookings.php">New</a></li>
+						<li><a href="confirmed-bookings.php">Confirmed</a></li>
+						<li><a href="canceled-bookings.php">Canceled</a></li>
+					</ul>
+				</li>
+
+		
+
+
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+
+			</ul>
+		</nav>
